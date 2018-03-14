5823ff47b83d1c8701a811d1b58823b312bcf727
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-03-14 07:49:01 +0600

    URI codes

---
+++ Accepted/Beginner/1042.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+
+    if(a>b){
+        if(a>c){
+            if(b>c){
+                printf("%d\n%d\n%d\n",c,b,a);
+            }
+            else{
+                printf("%d\n%d\n%d\n",b,c,a);
+            }
+        }
+        else{
+            printf("%d\n%d\n%d\n",b,a,c);
+        }
+    }
+    else{
+        if(b>c){
+            if(a>c){
+                printf("%d\n%d\n%d\n",c,a,b);
+            }
+            else{
+                printf("%d\n%d\n%d\n",a,c,b);
+            }
+        }
+        else{
+            printf("%d\n%d\n%d\n",a,b,c);
+        }
+    }
+
+    printf("\n");
+    printf("%d\n%d\n%d\n",a,b,c);
+
+    return 0;
+}
