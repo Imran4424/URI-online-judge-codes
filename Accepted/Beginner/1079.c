737a1fae3f6f1b66bc986fbc08c0ded0abf154db
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-10 15:33:20 +0600

    uri codes

--- Accepted/Beginner/1079.c
+++ Accepted/Beginner/1079.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int N,i;
+    scanf("%d",&N);
+    float a[N],b[N],c[N],ave[N];
+    for(i=0;i<N;i++){
+        scanf("%f%f%f",&a[i],&b[i],&c[i]);
+    }
+    for(i=0;i<N;i++){
+        ave[N]=(a[i]*2+b[i]*3+c[i]*5)/(2+3+5);
+
+        printf("%.1f\n",ave[N]);
+    }
+    return 0;
+}
 
